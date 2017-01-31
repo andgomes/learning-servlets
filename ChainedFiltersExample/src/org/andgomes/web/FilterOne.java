b64549f28a863eb03183ec6a15dfff562d3fb020
Anderson Gomes <gomes.andrade1@gmail.com>
2017-01-31 12:10:34 -0300

    Declarando e implementando Filters

---
+++ ChainedFiltersExample/src/org/andgomes/web/FilterOne.java
@@ -0,0 +1,33 @@
+package org.andgomes.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class FilterOne implements Filter {
+
+	private FilterConfig filterConfig;
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.filterConfig = filterConfig;
+	}
+	
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws ServletException, IOException {
+	
+		filterConfig.getServletContext().log("FilterOne's doFilter()");
+		
+		chain.doFilter(request, response);
+	
+	}
+	
+	public void destroy() {
+		// nothing here
+	}
+
+}
